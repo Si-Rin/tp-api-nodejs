daefb2b1721991b03473e6bb404e08bae59595e5
Sirin GRIRA <grirasirin@gmail.com>
2026-02-13 11:10:51 +0100

    Branche B: ajout commentaire application

--- server.js
+++ server.js
@@ -2,7 +2,10 @@
 // ============================================
 // IMPORTS
 // ============================================
+
+// Branche B : Application Express
 const express = require('express');
+
 const dotenv = require('dotenv');
 const connectDB = require('./config/database');
 
